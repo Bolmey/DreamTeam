15792c1e97952807b33ddc053f45a9f3125e3f74
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-07-03 09:13:44 -0400

    Update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,22 +4,24 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
-import logo from '../../images/dreamteamLogo.png'
 
 export default function NavbarSection() {
     return (
         <div className='NavbarContainer'>
             <Navbar collapseOnSelect expand="lg" className="navbarColor">
                 <Container>
+                    {/* <Link to="/"><Navbar className='navName' href="#home">Dream Team Trading</Navbar></Link> */}
+                    {/* -----this code is for a logo----- */}
+
                     <Link to="/"><Navbar.Brand href="#home">
                         <img
                             alt=""
                             src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
+                            className="d-inline-block align-top navLogo"
                         />
                     </Navbar.Brand></Link>
+
+                    {/* -----this code is for a logo----- */}
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav>
